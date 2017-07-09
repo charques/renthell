a4f606dc851c057862b3e7cb8a72b1de634dc67e
Carlos Felipe Hernandez Arques <carloshernandezarques@gmail.com>
2017-07-09 21:53:14 +0200

    feature: data extractor initial commit

---
+++ data-extractor/src/main/java/io/renthell/dataextractor/DataExtractorApplication.java
@@ -0,0 +1,12 @@
+package io.renthell.dataextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataExtractorApplication.class, args);
+	}
+}
