d549c45b1bd14345432b0d69fc2ba5cce86bbb18
Carlos Felipe Hernandez Arques <carloshernandezarques@gmail.com>
2017-07-23 10:56:56 +0200

    feature: event handler base

---
+++ event-handler-srv/src/main/java/io/renthell/eventhandlersrv/EventHandlerSrvApplication.java
@@ -0,0 +1,12 @@
+package io.renthell.eventhandlersrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHandlerSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHandlerSrvApplication.class, args);
+	}
+}
