d87013e783e0532439b8da9129792f0bfac4d286
Carlos Felipe Hernandez Arques <carloshernandezarques@gmail.com>
2017-08-21 23:56:41 +0200

    feature: build script styles

--- build.sh
+++ build.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+yel=$'\e[1;33m'
+blu=$'\e[1;34m'
+mag=$'\e[1;35m'
+cyn=$'\e[1;36m'
+end=$'\e[0m'
+
 cd event-store-srv
 ./mvnw clean package docker:build
-STATUS=$?
-if [ $STATUS -eq 0 ]; then
-echo "event-store-srv => docker image creation success"
-else
-echo "event-store-srv => docker image creation failed"
-fi
+STATUS_EVENT_STORE=$?
 
 cd ..
 cd property-mgmt-srv
 ./mvnw clean package docker:build
-STATUS=$?
-if [ $STATUS -eq 0 ]; then
-echo "property-mgmt-srv => docker image creation success"
-else
-echo "property-mgmt-srv => docker image creation failed"
-fi
+STATUS_PROPERTY_MGMT=$?
 
 cd ..
 cd scoring-mgmt-srv
 ./mvnw clean package docker:build
-STATUS=$?
-if [ $STATUS -eq 0 ]; then
-echo "scoring-mgmt-srv => docker image creation success"
+STATUS_SCORING_MGMT=$?
+echo
+echo "[${blu}INFO${end}] ------------------------------------------------------------------------"
+echo "[${blu}INFO${end}] ${mag}BUILD STATUS${end}"
+echo "[${blu}INFO${end}] ------------------------------------------------------------------------${end}"
+echo
+if [ $STATUS_EVENT_STORE -eq 0 ]; then
+echo "[${blu}INFO${end}] --- ${blu}event-store-srv${end}   => docker image creation ${grn}SUCCESS${end}"
 else
-echo "scoring-mgmt-srv => docker image creation failed"
+echo "[${blu}INFO${end}] --- ${blu}event-store-srv${end}   => docker image creation ${red}FAILED${end}"
 fi
+
+if [ $STATUS_PROPERTY_MGMT -eq 0 ]; then
+echo "[${blu}INFO${end}] --- ${blu}property-mgmt-srv${end} => docker image creation ${grn}SUCCESS${end}"
+else
+echo "[${blu}INFO${end}] --- ${blu}property-mgmt-srv${end} => docker image creation ${red}FAILED${end}"
+fi
+
+if [ $STATUS_SCORING_MGMT -eq 0 ]; then
+echo "[${blu}INFO${end}] --- ${blu}scoring-mgmt-srv${end}  => docker image creation ${grn}SUCCESS${end}"
+else
+echo "[${blu}INFO${end}] --- ${blu}scoring-mgmt-srv${end}  => docker image creation ${red}FAILED${end}"
+fi
+echo
